45deddd6daf368274052faff1d25e65ed7b63c0a
Itsuren Kanakueta <itsurenkanakueta@gmail.com>
2024-03-20 13:48:46 +0545

    labels Added

--- Java/Main.java
+++ Java/Main.java
@@ -1,6 +1,14 @@
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+
 public class Main {
     public static void main(String[] args){
+        ImageIcon image = new ImageIcon("../NOTES/Resources/icon.png");
+        JLabel label = new JLabel(); //create Label
+        //JLabel label = new JLabel("Hello World"); //shorter way to do instead of setText
+        label.setText("Hello World"); //Set Text of Label
+        label.setIcon(image); // setting icon to Label
         Window window = new Window();
-
+        window.add(label);
     }
 }
