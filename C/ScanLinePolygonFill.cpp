c6fa3873d37aab839af4a54d18ba0ca9745aff56
Itsuren Kanakueta <itsurenkanakueta@gmail.com>
2024-04-26 10:50:43 +0545

    Scan Line Polygon Fill Algorithm Added

---
+++ C/ScanLinePolygonFill.cpp
@@ -0,0 +1,94 @@
+#include <stdio.h>
+#include <graphics.h>
+#include <stdlib.h>
+#include "./Modules/Graphics.cpp"
+
+#define MAX_SIZE 50
+#define MAX_SCANLINES 500
+
+void PolygonInputs(Coordinates_Int EdgeList[], int *NumberofEdges, float Slopes[]) {
+    // Taking the Number of Edges
+    printf("\nPOLYGON DEFINITION: \n\nEnter the number of edges of Polygon: ");
+    scanf("%d", NumberofEdges);
+    // Entering the Edges to the Array
+    printf("\nEnter the coordinates of Polygon: \n");
+    for (int i=0; i < *NumberofEdges; i++) {
+        printf("\tX%d Y%d: ", i,i);
+        scanf("%d %d", &EdgeList[i].x, &EdgeList[i].y);
+    }
+    // Setting the End point to the First Point
+    EdgeList[*NumberofEdges].x = EdgeList[0].x;
+    EdgeList[*NumberofEdges].y = EdgeList[0].y;
+
+
+    // Slope Setups
+    Coordinates_Int Difference;
+    for (int i=0; i < *NumberofEdges; i++) {
+       Difference.x = EdgeList[i+1].x - EdgeList[i].x;
+       Difference.y = EdgeList[i+1].y - EdgeList[i].y;
+        if (Difference.y == 0) {
+            Slopes[i] = 1.0;
+        } else if (Difference.x == 0) {
+            Slopes[i] = 0.0;
+        } else {
+            Slopes[i] = (float) Difference.x / Difference.y;
+        }
+    }
+}
+
+void PolygonDraw(Coordinates_Int EdgeList[], int NumberofEdges) {
+    for (int i=0; i < NumberofEdges; i++) {
+        line(EdgeList[i].x, EdgeList[i].y, EdgeList[i+1].x, EdgeList[i+1].y);
+    }
+}
+
+void X_IntersectionFindFill(Coordinates_Int EdgeList[], int NumberofEdges, int ScanLine, float Slopes[]) {
+    // For a Given Scan Line
+    int xIntercepts[MAX_SIZE];
+    int NumberofIntercepts=0;
+    for (int i=0; i < NumberofEdges; i++) {
+        // Listing Intersections on a scanline
+        // ScanLine Separates Two adjacentPoints in incresing slopr or decreasing slope which means there is an intersection
+        if ( ((EdgeList[i].y <= ScanLine) && (EdgeList[i+1].y>ScanLine)) || ((EdgeList[i].y > ScanLine) && (EdgeList[i+1].y<=ScanLine))) { 
+            // x intercept = x + (Δx/Δy) * (scanline -y);
+            xIntercepts[NumberofIntercepts] = (int) (EdgeList[i].x + Slopes[i]*(ScanLine - EdgeList[i].y) );
+            NumberofIntercepts++;
+        }
+    }
+
+    // Sorting Intersections
+    for (int i=0; i < NumberofIntercepts-1; i++) {
+        for ( int j=i+1; j < NumberofIntercepts; j++) {
+            if (xIntercepts[i] > xIntercepts[j] ) {
+                int temp = xIntercepts[i];
+                xIntercepts[i] = xIntercepts[j];
+                xIntercepts[j] = temp;
+            }
+        }
+    }
+    
+    // Drawing
+    for (int i=0; i < NumberofIntercepts; i+=2) {
+        delay(15);
+        line(xIntercepts[i], ScanLine, xIntercepts[i+1]+1, ScanLine);
+    }
+}
+
+int main() {
+    system("cls");
+    int NumberofEdges; float Slopes[MAX_SIZE];
+    Coordinates_Int EdgeList[MAX_SIZE];
+
+    PolygonInputs(EdgeList, &NumberofEdges, Slopes); 
+
+    graphicsinits();
+    PolygonDraw( EdgeList, NumberofEdges);
+
+    // Scan Lines Rasterizing
+    for (int Scanline=0; Scanline < MAX_SCANLINES; Scanline++) {
+        X_IntersectionFindFill(EdgeList, NumberofEdges, Scanline, Slopes);
+    }
+
+    getch();
+    closegraph();
+}
